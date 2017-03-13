9caf271c5eee072a72870a68d991e0462213451e
Armando Lizarraga <armandolv@gmail.com>
2017-03-13 16:23:12 -0700

    Update module2 solution

--- module2-solution/js/app.js
+++ module2-solution/js/app.js
@@ -3,18 +3,52 @@
 
   angular.module('ShoppingListCheckOff', [])
          .controller('ToBuyController', ToBuyController)
-         .controller('AlreadyBoughtController', AlreadyBoughtController);
+         .controller('AlreadyBoughtController', AlreadyBoughtController)
+         .service('ShoppingListCheckOffService', ShoppingListCheckOffService);
 
-  ToBuyController.$inject = ['$scope'];
+  ToBuyController.$inject = ['ShoppingListCheckOffService'];
 
-  function ToBuyController($scope) {
+  function ToBuyController(ShoppingListCheckOffService) {
+    var tobuy = this;
+
+    tobuy.itemsToBuy = ShoppingListCheckOffService.getItemsToBuy();
+
+    tobuy.buyItem = function (itemIndex) {
+      ShoppingListCheckOffService.buy(itemIndex);
+    }
 
   }
 
-  AlreadyBoughtController.$inject = ['$scope'];
+  AlreadyBoughtController.$inject = ['ShoppingListCheckOffService'];
   
-  function AlreadyBoughtController($scope) {
+  function AlreadyBoughtController(ShoppingListCheckOffService) {
+    var bought = this;
 
+    bought.itemsBought = ShoppingListCheckOffService.getItemsBought();
+  }
+
+  function ShoppingListCheckOffService() {
+    var service = this;
+
+    var itemsToBuy = [{ name: "cookies", quantity: 10 }, 
+                      { name: "Bottles of water", quantity: 12 }, 
+                      { name: "Cakes", quantity: 13 }, 
+                      { name: "Onions", quantity: 8 }, 
+                      { name: "Tables", quantity: 4 }];
+    var itemsBought = [];
+
+    service.getItemsToBuy = function () {
+      return itemsToBuy;
+    };
+
+    service.getItemsBought = function () {
+      return itemsBought;
+    };
+
+    service.buy = function (index) {
+      itemsBought.push(itemsToBuy[index])
+      itemsToBuy.splice(index, 1);
+    }
   }
 
 })();
