a15ceb0e99703cb08ea22b92bf0018d35d5996e9
Armando Lizarraga <armandolv@gmail.com>
2017-03-12 22:52:36 -0700

    Add module 2 solution incomplete

---
+++ module2-solution/js/app.js
@@ -0,0 +1,20 @@
+(function () {
+  'use strict';
+
+  angular.module('ShoppingListCheckOff', [])
+         .controller('ToBuyController', ToBuyController)
+         .controller('AlreadyBoughtController', AlreadyBoughtController);
+
+  ToBuyController.$inject = ['$scope'];
+
+  function ToBuyController($scope) {
+
+  }
+
+  AlreadyBoughtController.$inject = ['$scope'];
+  
+  function AlreadyBoughtController($scope) {
+
+  }
+
+})();
