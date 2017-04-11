9d784facc6132423d8e422f1e2995c9b2d9c9af1
Armando Lizarraga <armandolv@gmail.com>
2017-04-10 23:20:53 -0600

    Update module 4 solution

--- module4-solution/src/items.controller.js
+++ module4-solution/src/items.controller.js
@@ -5,8 +5,8 @@
   .controller('ItemsController', ItemsController);
 
 
-  ItemsController.$inject = ['$stateParams', 'items'];
-  function ItemsController($stateParams, items) {
+  ItemsController.$inject = ['items'];
+  function ItemsController(items) {
     var itemCtrl = this;
     itemCtrl.items = items['menu_items'];
     itemCtrl.category = items['category'];
